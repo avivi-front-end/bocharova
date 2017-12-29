d4e25f68824dca3cac97e02ad09a7a2cfcca18db
trolizmaslom <trolizmaslom@gmail.com>
2017-12-29 17:25:55 +0200

    ваі

--- src/js/develop.js
+++ src/js/develop.js
@@ -1,24 +1,5 @@
-
-$('.site-nav').removeClass('show'); //for hide mobile navigation
-$('.site-header__box').removeClass('show'); //for hide mobile navigation
-
-$(document).ready(function(){
-    $(".logo").delay(1000).fadeIn('slow');
-    $(".section-1__head").delay(1500).fadeIn('slow');
-    $(".button-contact-us").delay(1500).fadeIn('slow');
-    $("#scroll").delay(4000).fadeIn('slow');
-    // random padding for catalog items
-    function RandomPaddingCatalog() {
-        $(".random-padding-top-js").each(function() {
-            var random = Math.round(Math.random() * 50) + 100;
-            $(this).css("padding-top",random);
-            console.log(random + "px")
-        });
-    }
-    //RandomPaddingCatalog();
-});
-
-//slick slider
+var prevAr = '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>';
+var nextAr='<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>';
 $(document).ready(function(){
     $('.main-slider').slick({
         slidesToShow: 1,
@@ -32,25 +13,10 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        // nextArrow: '<img class="slider-right" src="../images/slider-right.svg" alt="" width="16" height="19">',
+        prevArrow:prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false
-                }
-            }
+            {breakpoint: 600,settings: {arrows: false }}
         ]
     });
     $('.gallery').slick({
@@ -58,47 +24,14 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="30px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="30px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        // nextArrow: '<img class="slider-right" src="../images/slider-right.svg" alt="" width="16" height="19">',
+        prevArrow:prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1800,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1367,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1359,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 970,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-            ,
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: true
-                }
-            }
+            { breakpoint: 1800, settings: {slidesToShow: 5}},
+            { breakpoint: 1367, settings: {slidesToShow: 4}},
+            { breakpoint: 1359, settings: {slidesToShow: 3}},
+            { breakpoint: 970, settings: {slidesToShow: 2}},
+            { breakpoint: 640, settings: {slidesToShow: 1, arrows: true}}
         ]
     });
     $('.order-conditions').slick({
@@ -106,37 +39,11 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
+        prevArrow:prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1800,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1359,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 970,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 680,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
+            {breakpoint: 970,settings: { slidesToShow: 2 }},
+            {breakpoint: 680,settings: {slidesToShow: 1}}
         ]
     });
     $('.gallery-footer').slick({
@@ -144,46 +51,14 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        // nextArrow: '<img class="slider-right" src="../images/slider-right.svg" alt="" width="16" height="19">',
+        prevArrow:prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1800,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1370,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 970,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: true
-                }
-            }
+            {breakpoint: 1800,settings: { slidesToShow: 5}},
+            {breakpoint: 1370,settings: {slidesToShow: 4}},
+            {breakpoint: 1200, settings: { slidesToShow: 3 }},
+            { breakpoint: 970,  settings: {  slidesToShow: 2 }},
+            { breakpoint: 640, settings: {slidesToShow: 1, arrows: true}}
         ]
     });
     $('.catalog-slick').slick({
@@ -191,39 +66,12 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        // nextArrow: '<img class="slider-right" src="../images/slider-right.svg" alt="" width="16" height="19">',
+        prevArrow: prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1800,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1359,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 970,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 650,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: true
-                }
-            }
+            {breakpoint: 1359,settings: { slidesToShow: 3}},
+            {breakpoint: 970,settings: {slidesToShow: 2}},
+            {breakpoint: 650,settings: {slidesToShow: 1, arrows: true}}
         ]
     });
     $('.goods-view__slider_big').slick({
@@ -242,14 +90,7 @@ $(document).ready(function(){
         centerPadding: '5px',
         focusOnSelect: true,
         responsive: [
-            {
-                breakpoint: 1500,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    centerPadding: '0'
-                }
-            }
+            {breakpoint: 1500, settings: {centerPadding: '0'}}
         ]
     });
     $('.recommended-products__box').slick({
@@ -257,154 +98,66 @@ $(document).ready(function(){
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        prevArrow: '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
-        nextArrow: '<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px" transform="rotate(180)"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>',
+        prevArrow:prevAr,
+        nextArrow: nextAr,
         responsive: [
-            {
-                breakpoint: 1500,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-            ,
-            {
-                breakpoint: 710,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
+            {breakpoint: 1500,settings: {slidesToShow: 3}},
+            {breakpoint: 1024,settings: {slidesToShow: 2}},
+            {breakpoint: 710,settings: {slidesToShow: 1}}
         ]
     });
-
-});
-
-
-//mobile menu
-$(document).ready(function(){
+    $('.section-1__search').click(function() {
+        if($(window).width()<600){
+            $('.section-1__search').addClass('section-1__search-js');
+            $('.logo').css("visibility", "hidden");
+        }
+    });
+    $('.header__search').click(function() {
+        if($(window).width()<600){
+            $('.header__search').addClass('search-form-js');
+            $('.site-header__logo').css("visibility", "hidden");
+        }
+    });
+    var element = $('#header-main');
+    var section2 = $('.section-2');
+    var elementHeight = element.height();
+    var height = $(window).height() - elementHeight;
     $('.toggle').on('click', function(){
         $('.site-nav').toggleClass('show');
         $('.social-network').toggleClass('social-network-mobile');
         $('.lang').toggleClass('lang-mobile');
     });
-
     $('.toggle-1').on('click', function(){
         $('.site-header__box').toggleClass('show-1');
     });
-});
-
-// mobile search form section-1
-$('.section-1__search').click(function() {
-    if($(window).width()<600){
-        $('.section-1__search').addClass('section-1__search-js');
-        $('.logo').css("visibility", "hidden");
-    }
-});
-// mobile search form header
-$('.header__search').click(function() {
-    if($(window).width()<600){
-        $('.header__search').addClass('search-form-js');
-        $('.site-header__logo').css("visibility", "hidden");
-    }
-});
-
-// $(document).ready(function(){
-//     $('.site-header').affix({
-//         offset: {
-//             top: $('.section-1').height()-$('.site-header').height()
-//         }
-//     });
-// });
-
-//fixed and fadein navigation
-$(document).ready(function(){
-    var element = $('#header-main'); //.site-header
-    var section1 = $('.wrap-section-1');
-    var section2 = $('.section-2');
-    var elementHeight = element.height();
-    var height = $(window).height() - elementHeight; //$(window).height() - 70
-    // var body = $('body');
-    // var bodyHeight = body.outerHeight(true);
-    // var scrolledBlock = $('.scrolled-block');
-    // var sbWidth = scrolledBlock.width();
-    // var winHeight = $(window).height();
-    // var maxScroll = bodyHeight - winHeight;
-    // var coeff = maxScroll/(sbWidth-elementWidth);
     $(window).scroll(function() {
         var $this = $(this);
-
-        if ($this.scrollTop() > height) { //($this.scrollTop() > 960)
-            // section1.css("display", "none"); // для анімаціїї при скролі вверх
+        if ($this.scrollTop() > height) {
             section2.css("margin-top", "100vh");
             element.addClass('header-fixed').css("opacity", "1");
             $('.section-1__header').css("opacity", "0");
         } else {
-            // section1.css("display", "block"); // для анімаціїї при скролі вверх
             section2.css("margin-top", "0");
             element.removeClass('header-fixed').css("opacity", "0");
             $('.section-1__header').css("opacity", "1");
         }
     });
 });
-
-// /*GO TO href*/
-var goTo = (function(){
-    $('.js-scroll-link').click(function(e){
-        e.preventDefault();
-        var href = $(this).attr('href');
-        var target = $(href).offset().top-65;
-        $('body,html').animate({scrollTop: target}, 1500);
-    });
-})();
-
-
-// animate catalog
-// $(document).ready(function(){
-//     var element = $('.catalog__item');
-//     var height = $(window).height() * 4 -200;
-//
-//     $(window).scroll(function() {
-//         var $this = $(this);
-//
-//         if ($this.scrollTop() > height) {
-//             element.addClass('catalog__animate-2');
-//             // element.css("right", "0");
-//         } else {
-//             element.removeClass('catalog__animate-2');
-//         }
-//     });
-// });
+$(window).on('load', function () {
+    $('.preloader').css('opacity','0');
+    setTimeout(function () {
+        $('.preloader').remove();
+    },200);
+    $('.site-nav').removeClass('show');
+    $('.site-header__box').removeClass('show');
+    $(".logo").delay(500).fadeIn(500);
+    setTimeout(function () {
+        $(".section-1__head").addClass('animate-this');
+    },1000);
+    setTimeout(function () {
+        $(".button-contact-us").addClass('animate-this');
+    },1600);
 
 
-// jQuery(function(f){
-//     var element = f('.button-contact-us');
-//     f(window).scroll(function(){
-//         element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](500);
-//     });
-// });
-
-
-// $(window).on('load', function () {
-//     var $preloader = $('.button-contact-us'),
-//         $spinner   = $preloader.find('.spinner');
-//     $spinner.fadeOut();
-//     $preloader.delay(350).fadeOut('slow');
-// });
-//
-
-
-// //зникнення елемента при прокрутці  http://gnatkovsky.com.ua/poyavlenie-ischeznovenie-elementa-pri-prokrutke-stranicy-jquery.html
-// jQuery(function(f){
-//     var element = f('.button-contact-us');
-//     f(window).scroll(function(){
-//         element['fade'+ (f(this).scrollTop() > 200 ? 'In': 'Out')](500);
-//     });
-// });
+    $("#scroll").delay(700).fadeIn(200);
+});
