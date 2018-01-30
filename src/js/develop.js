9e113227780d5b1eae1b6c6c6764396afe8ce84e
trolizmaslom <trolizmaslom@gmail.com>
2018-01-30 08:37:47 +0200

    h

--- src/js/develop.js
+++ src/js/develop.js
@@ -2,6 +2,7 @@ var prevAr = '<div class="slider-left"><svg xmlns="http://www.w3.org/2000/svg" x
 var nextAr='<div class="slider-right"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17px" height="31px"  opacity="0.4"> <path fill-rule="evenodd"  fill="rgb(0, 0, 0)"d="M15.092,30.753 L17.002,28.745 L3.930,15.749 L17.002,2.756 L15.093,0.745 L-0.002,15.749 L15.092,30.753 Z"/> </svg></div>';
 var opened = false;
 $(document).ready(function(){
+    $.fancybox.defaults.touch = false;
     $('.main-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
